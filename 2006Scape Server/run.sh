8c1dd90393be113eefbd07863b897d89cc60ffd6
Adam <riizade@gmail.com>
2024-07-24 10:43:13 -0400

    run scripts

---
+++ 2006Scape Server/run.sh
@@ -0,0 +1,1 @@
+java -jar Server.jar -c ServerConfig.json -gui
---
+++ build.sh
@@ -0,0 +1,3 @@
+mvn -B clean install
+cp "2006Scape Client\target\client-1.0-jar-with-dependencies.jar" "2006Scape Client\Client.jar"
+cp "2006Scape Server/target/server-1.0-jar-with-dependencies.jar" "2006Scape Server\Server.jar"
